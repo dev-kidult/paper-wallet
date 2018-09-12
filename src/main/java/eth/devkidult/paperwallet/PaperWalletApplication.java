f47460eb4f0fd67649c58f9a40dcf76fa5bca237
dev-kidult <doole3488@gmail.com>
2018-09-12 19:18:39 +0900

    spring project init

---
+++ src/main/java/eth/devkidult/paperwallet/PaperWalletApplication.java
@@ -0,0 +1,12 @@
+package eth.devkidult.paperwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperWalletApplication.class, args);
+    }
+}
